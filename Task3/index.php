95e542b7e2baf2c8bcd084cced40e5ca818c7d82
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-07-19 14:57:20 +0200

    Update index.php

--- Task3/index.php
+++ Task3/index.php
@@ -22,40 +22,67 @@
     
     $x = $_GET["x"] ?? null;
     $y = $_GET["y"] ?? null;
+    
     $pressure = $_GET["pressure"] ?? null;
     $humidity = $_GET["humidity"] ?? null;
     $temperature = $_GET["temperature"] ?? null;
-    $RGB = [$_GET["R"] ?? null, $_GET["G"] ?? null, $_GET["B"] ?? null] ;
+    
     $R = $_GET["R"] ?? null;
     $G = $_GET["G"] ?? null;
     $B = $_GET["B"] ?? null;
+    $RGB = [$R, $G, $B];
     $RGB_by_element = array_map('htmlspecialchars', $RGB);
     
+    $gyro_x = $_GET["gyro_x"] ?? null;
+    $gyro_y = $_GET["gyro_y"] ?? null;
+    $gyro_z = $_GET["gyro_z"] ?? null;
+    $gyro = [$gyro_x, $gyro_y, $gyro_z];
     
-    $keys = array("x", "y", "pressure", "humidity", "temperature", "R", "G", "B");
+    $magnet_x = $_GET["magnet_x"] ?? null;
+    $magnet_y = $_GET["magnet_y"] ?? null;
+    $magnet_z = $_GET["magnet_z"] ?? null;
+    $magnet = [$magnet_x, $magnet_y, $magnet_z];
+    
+    $accel_x = $_GET["accel_x"] ?? null;
+    $accel_y = $_GET["accel_y"] ?? null;
+    $accel_z = $_GET["accel_z"] ?? null;
+    $accel = [$accel_x, $accel_y, $accel_z];
+    
+    
+    $keys = array("x", "y", "pressure", "humidity", "temperature", "R", "G", "B",
+                  "gyro_x", "gyro_y", "gyro_z", "magnet_x", "magnet_y", "magnet_z",
+                  "accel_x", "accel_y", "accel_z");
+    $sensor_keys = array("IMU", "Sensors", "RGB matrix");
+    
+    $IMU = array();
+    $Sensors = array();
+    $RGB_matrix = array();
+    
     $arr = array();
     $file = 'C:\Users\limok\OneDrive\Рабочий стол\JSON.txt';
     if(isset($x) || isset($y) || isset($pressure) || isset($humidity) || isset($temperature)
-            || isset($RGB)){         
+            || isset($RGB) || isset($gyro) || isset($magnet) || isset($accel)){         
         if(htmlspecialchars($x) == NULL){
            echo "<pre>". "Value of x coordinate of RGB matrix: "."Please, set the value". "<br>";
         }
-        else if($_GET["x"] >= 8 || $_GET["x"] <= 1){
+        else if($x > 8 || $x < 1){
            echo "<pre>". "Value of x coordinate of RGB matrix: "."Value is out of range. Please, try again."."<br>";
         }
-        else if($_GET["x"] <= 8 && $_GET["x"] >= 1){
+        else if($x <= 8 && $x >= 1){
             echo "<pre>". "Value of x coordinate of RGB matrix: ". htmlspecialchars($x);
         }
         
         if(htmlspecialchars($y) == NULL){
            echo "<pre>". "Value of y coordinate of RGB matrix: "."Please, set the value". "<br>";
         }
-        else if($_GET["y"] >= 8 || $_GET["y"] <= 1){
+        else if($y > 8 || $y < 1){
            echo "<pre>". "Value of y coordinate of RGB matrix: "."Value is out of range. Please, try again."."<br>";
         }
-        else if($_GET["y"] <= 8 && $_GET["y"] >= 1){
+        else if($y <= 8 && $y >= 1){
             echo "<pre>". "Value of y coordinate of RGB matrix: ". htmlspecialchars($y);
         }
+        
+        
         echo "<pre>". "Pressure: ". htmlspecialchars($pressure);
         if(htmlspecialchars($pressure) == NULL){
            echo "Please, set the value". "<br>";
@@ -68,35 +95,78 @@
         if(htmlspecialchars($temperature) == NULL){
            echo "Please, set the value". "<br>";
         }
-        if(($R <= 255 && $R >= 0) && ($G <= 255 && $G >= 0) && ($B <= 255 && $B >= 0)){
+        
+        
+        if(is_null($R) && is_null($G) && is_null($B)){
+            echo "<pre>". "RGB: "."Please, set the value". "<br>";
+        }
+        else if(($R <= 255 && $R >= 0) && ($G <= 255 && $G >= 0) && ($B <= 255 && $B >= 0)){
             echo "<pre>". "RGB: ". "R=".$R, ", G=".$G, ", B=".$B;
         }
-        else if($RGB == NULL){
-            echo "Please, set the value". "<br>";
+        else if(($R > 255 || $R < 0) || ($G > 255 || $G <0) || ($B > 255 || $B < 0)){
+            echo "<pre>". "RGB: "."Value is out of range. Please, try again". "<br>";
         }
-        else{
-            echo "Value is out of range. Please, try again". "<br>";
+        
+        
+        if(is_null($gyro_x) && is_null($gyro_y) && is_null($gyro_z)){
+            echo "<pre>". "Gyroscope: "."Please, set the value". "<br>";
+        }
+        else if((floatval($gyro_x) <= 245 && floatval($gyro_x) >= -245) && (floatval($gyro_y) <= 245 && floatval($gyro_y) >= -245) && (floatval($gyro_z) <= 245 && floatval($gyro_z) >= -245)){
+            echo "<pre>". "Gyroscope: ". "x=".$gyro_x, ", y=".$gyro_y, ", z=".$gyro_z;
+        }
+        else if((floatval($gyro_x) > 245 || floatval($gyro_x) < -245) || (floatval($gyro_y) > 245 || floatval($gyro_y) < -245) || (floatval($gyro_z) > 245 || floatval($gyro_z) < -245)){
+            echo "<pre>"."Gyroscope: "."Value is out of range. Please, try again". "<br>";
+        }
+        
+        
+        if(is_null($magnet_x) && is_null($magnet_y) && is_null($magnet_z)){
+            echo "<pre>". "Magnetometer: "."Please, set the value". "<br>";
+        }        
+        else if((floatval($magnet_x) <= 400 && floatval($magnet_x) >= -400) && (floatval($magnet_y) <= 400 && floatval($magnet_y) >= -400) && (floatval($magnet_z) <= 400 && floatval($magnet_z) >= -400)){
+            echo "<pre>". "Magnetometer: ". "x=".$magnet_x, ", y=".$magnet_y, ", z=".$magnet_z;
+        }
+        else if((floatval($magnet_x) > 400 || floatval($magnet_x) < -400) || (floatval($magnet_y) > 400 || floatval($magnet_y) < -400) || (floatval($magnet_z) > 400 || floatval($magnet_z) < -400)){
+            echo "<pre>"."Magnetometer: "."Value is out of range. Please, try again". "<br>";
+        }
+        
+        
+        if(is_null($accel_x) && is_null($accel_y) && is_null($accel_z)){
+            echo "<pre>". "Accelerometer: "."Please, set the value". "<br>";
+        }        
+        else if((floatval($accel_x) <= 2 && floatval($accel_x) >= -2) && (floatval($accel_y) <= 2 && floatval($accel_y) >= -2) && (floatval($accel_z) <= 2 && floatval($accel_z) >= -2)){
+            echo "<pre>". "Accelerometer: ". "x=".$magnet_x, ", y=".$magnet_y, ", z=".$magnet_z;
+        }
+        else if((floatval($accel_x) > 2 || floatval($accel_x) < -2) || (floatval($accel_y) > 2 || floatval($accel_y) < -2) || (floatval($accel_z) > 2 || floatval($accel_z) < -2)){
+            echo "<pre>"."Accelerometer: "."Value is out of range. Please, try again". "<br>";
         }
     }
     
    
     foreach($keys as $key){
         if(isset($_GET[$key])){
-            if(($R <= 255 && $R > 0) || ($G <= 255 && $G > 0) || ($B <= 255 && $B > 0) ||
-               ($x <= 8 && $x > 0) || ($y <= 8 && $y > 0) || 
-               ($pressure <= 1260 && $pressure > 260) || ($humidity <= 100 && $humidity > 0) || ($temperature <= 120 && $temperature > -40))
-            {
+
                $array["value"] = $_GET[$key];
                $arr[$key] = $array;
-            }
-            else 
-                ;
+                if (in_array($key, ["R", "G", "B"]) && ($_GET[$key] <= 255 && $_GET[$key] > 0) ||
+                    in_array($key, ["x", "y"]) && ($_GET[$key] <= 8 && $_GET[$key] > 0)) {
+                       $RGB_matrix["RGB matrix"][$key] = ["value" => $_GET[$key]];
+                } 
+                else if(in_array($key, ["temperature"]) && ($_GET[$key] <= 120 && $_GET[$key] > -40) ||
+                        in_array($key, ["humidity"]) && ($_GET[$key] <= 100 && $_GET[$key] > 0) ||
+                        in_array($key, ["pressure"]) && ($_GET[$key] <= 1260 && $_GET[$key] > 260)) {
+                         $Sensors["Sensors"][$key] = ["value" => $_GET[$key]];
+                }
+                else if(in_array($key, ["gyro_x", "gyro_y", "gyro_z"]) && ($_GET[$key] <=245 && $_GET[$key] >= -245) ||
+                        in_array($key, ["magnet_x", "magnet_y", "magnet_z"]) && ($_GET[$key] <= 400 && $_GET[$key] >= -400) ||
+                        in_array($key, ["accel_x", "accel_y", "accel_z"]) && ($_GET[$key] <=2 && $_GET[$key] >= -2)) {
+                         $IMU["IMU"][$key] = ["value" => $_GET[$key]];
+                }                
+                $total = array_merge($RGB_matrix, $Sensors, $IMU);
         }
     }
-   # print_r($array);
+
     
-    $json_encoded = json_encode($arr, JSON_PRETTY_PRINT);
-   # echo "JSON object output: " . "<pre>". $json_encoded . "<pre>";
+    $json_encoded = json_encode($total, JSON_PRETTY_PRINT);
     if(file_exists($file)){
         if($json_encoded == '[]'){
             $json_encoded = NULL;
